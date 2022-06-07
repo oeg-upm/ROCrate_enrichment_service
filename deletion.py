5ee22829c37e611759d1e33958f92019d0562f16
Geo <georgehadib@users.noreply.github.com>
2022-06-07 12:33:13 +0200

    New Commit 07/06/2022 Deletion script is created to be executed periodically

---
+++ deletion.py
@@ -0,0 +1,13 @@
+import shutil
+import glob
+
+import os
+paths = ["./pending_jobs/","./done_jobs/"]
+for path in paths:
+    for file_name in os.listdir(path):
+        # construct full file path
+        file = path + file_name
+        if os.path.isfile(file):
+            print('Deleting file:', file)
+            os.remove(file)
+        
