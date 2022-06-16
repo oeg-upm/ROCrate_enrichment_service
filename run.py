6cec3bcf0ac3311606baf5240d8ac0ad503e0902
Geo <georgehadib@users.noreply.github.com>
2022-06-16 10:38:21 +0200

    New Commit 16/06/2022 requirements.txt update

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-import os, sys, threading, time, enrichment, API_Server_v2, deletion
-import subprocess
-from apscheduler.schedulers.background import BackgroundScheduler
+import threading, enrichment, API_Server_v2
 from crontab import CronTab  
 
 
@@ -13,11 +11,7 @@ def run_deletion():
         task = cron.new(command='py deletion.py') 
         task.day.every(2)
         cron.write()
-        '''
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(deletion.delete_jobs, 'interval', hours=48)
-        scheduler.start()
-        '''
+       
         
 def run_enrichment():
         enrichment.enrich_RO()  
@@ -29,26 +23,7 @@ def run_server():
         print("API_Server_v2.py is up and running")
         
         
-'''
-subprocess.run("python enrichment.py &", shell=True)
-print("HERE")
 
-process1 = subprocess.Popen(r'.\\enrichment.py &',shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-my_pid, err = process1.communicate()
-
-if len(my_pid.splitlines()) >0:
-        sys.exit(0);
-
-
-process2 = subprocess.Popen(r'.\\API_Server_v2.py',shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-my_pid, err = process2.communicate()
-
-if len(my_pid.splitlines()) >0:
-        sys.exit(0);
-        
-'''
 
 t1= threading.Thread(target=run_server)
 t2= threading.Thread(target=run_enrichment)
